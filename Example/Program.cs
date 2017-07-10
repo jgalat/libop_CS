987a28a802238fd88fdd0c86b7672090f6d391e3
Jorge Galat <jrgglt@gmail.com>
2017-07-10 17:28:23 -0300

    Update
    - LibOp DllImports
    - Added Example

--- Example/Program.cs
+++ Example/Program.cs
@@ -8,15 +8,41 @@ using LibOpCS;
 
 namespace Example
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            IntPtr Option =
-                LibOp.NewOption(LibOp.OptionType.OPTION_CALL,
-                        LibOp.ExerciseType.EU_EXERCISE, IntPtr.Zero, 21.0);
+      IntPtr TP = LibOp.NewTimePeriod365();
+      double S = 21.0, K = 20.0;
+      double vol = 0.4;
+      double r = 0.09;
+      //IntPtr div = LibOp.NewContinuousDividend(0.1);
 
-            Thread.Sleep(5000);
-        }
+      IntPtr div = LibOp.NewDiscreteDividend();
+
+      int[] dates = { 182 } ;
+      double[] ammounts = { 1 };
+
+      LibOp.DiscDivSetDates(div, TP, 1, dates);
+      LibOp.DiscDivSetAmmounts(div, 1, ammounts);
+
+      IntPtr EuOption =
+        LibOp.NewOption(LibOp.OptionType.OPTION_CALL,
+          LibOp.ExerciseType.EU_EXERCISE, LibOp.DAYS(TP, 365), K);
+
+      IntPtr EuAnalytic =
+        LibOp.NewPricingMethod(LibOp.PricingMethodId.EU_ANALYTIC,
+          vol, r, div);
+
+      LibOp.OptionSetPricingMethod(EuOption, EuAnalytic);
+
+      IntPtr result = LibOp.NewResult();
+
+      LibOp.OptionPrice(EuOption, S, result);
+
+      Console.WriteLine(LibOp.ResultGetPrice(result));
+
+      Console.ReadLine();
     }
+  }
 }
--- LibOpCS/LibOp.cs
+++ LibOpCS/LibOp.cs
@@ -43,49 +43,49 @@ namespace LibOpCS
       * option.h 
       */
 
-    [DllImport(LibOp_dll, EntryPoint = "new_option")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_option")]
     public static extern
     IntPtr NewOption(OptionType option_type,
         ExerciseType exercise_type, IntPtr time_period, double strike);
 
-    [DllImport(LibOp_dll, EntryPoint = "delete_option")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "delete_option")]
     public static extern
     void DeleteOption(IntPtr option);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_set_pricing_method")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_set_pricing_method")]
     public static extern
     int OptionSetPricingMethod(IntPtr option, IntPtr pricing_method);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_price")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_price")]
     public static extern
     int OptionPrice(IntPtr option, double S, IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_delta")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_delta")]
     public static extern
     int OptionDelta(IntPtr option, double S, IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_gamma")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_gamma")]
     public static extern
     int OptionGamma(IntPtr option, double S, IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_theta")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_theta")]
     public static extern
     int OptionTheta(IntPtr option, double S, IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_rho")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_rho")]
     public static extern
     int OptionRho(IntPtr option, double S, IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_vega")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_vega")]
     public static extern
     int OptionVega(IntPtr option, double S, IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_impl_vol")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_impl_vol")]
     public static extern
     int OptionImpliedVolatility(IntPtr option, double V,
                                 double S, IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "option_price_and_greeks")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "option_price_and_greeks")]
     public static extern
     int OptionPriceAndGreeks(IntPtr option, double S, IntPtr result);
 
@@ -98,16 +98,16 @@ namespace LibOpCS
       AM_FD
     };
 
-    [DllImport(LibOp_dll, EntryPoint = "new_pricing_method")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_pricing_method")]
     public static extern
     IntPtr NewPricingMethod(PricingMethodId id, double volatility,
                             double risk_free_rate, IntPtr dividend);
 
-    [DllImport(LibOp_dll, EntryPoint = "delete_pricing_method")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "delete_pricing_method")]
     public static extern
     void DeletePricingMethod(IntPtr pricing_method);
 
-    [DllImport(LibOp_dll, EntryPoint = "delete_pricing_method")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "delete_pricing_method")]
     public static extern
     int PMSetSettings(IntPtr pricing_method, IntPtr PMSettings);
 
@@ -115,39 +115,39 @@ namespace LibOpCS
      * pm_settings.h
      */
 
-    [DllImport(LibOp_dll, EntryPoint = "new_pm_settings")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_pm_settings")]
     public static extern
     IntPtr NewPMSettings();
 
-    [DllImport(LibOp_dll, EntryPoint = "delete_pm_settings")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "delete_pm_settings")]
     public static extern
     void DeletePMSettings(IntPtr pm_settings);
 
-    [DllImport(LibOp_dll, EntryPoint = "pm_settings_set_grid_size")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pm_settings_set_grid_size")]
     public static extern
     int PMSettingsSetGridSize(IntPtr pm_settings, int grid_size);
 
-    [DllImport(LibOp_dll, EntryPoint = "pm_settings_set_Smax")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pm_settings_set_Smax")]
     public static extern
     int PMSettingsSetSMax(IntPtr pm_settings, double s_max);
 
-    [DllImport(LibOp_dll, EntryPoint = "pm_settings_set_tol")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pm_settings_set_tol")]
     public static extern
     int PMSettingsSetTol(IntPtr pm_settings, double tol);
 
-    [DllImport(LibOp_dll, EntryPoint = "pm_settings_set_abstol")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pm_settings_set_abstol")]
     public static extern
     int PMSettingsSetAbsTol(IntPtr pm_settings, double abstol);
 
-    [DllImport(LibOp_dll, EntryPoint = "pm_settings_set_iv_max_it")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pm_settings_set_iv_max_it")]
     public static extern
     int PMSettingsSetIVMaxIterations(IntPtr pm_settings, int max);
 
-    [DllImport(LibOp_dll, EntryPoint = "pm_settings_set_iv_eps")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pm_settings_set_iv_eps")]
     public static extern
     int PMSettingsSetIVTol(IntPtr pm_settings, double tol);
 
-    [DllImport(LibOp_dll, EntryPoint = "pm_settings_set_iv_init")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "pm_settings_set_iv_init")]
     public static extern
     int PMSettingsSetIVInitialGuesses(IntPtr pm_settings,
                                       double guess_vol0,
@@ -157,31 +157,31 @@ namespace LibOpCS
      * time_period.h
      */
 
-    [DllImport(LibOp_dll, EntryPoint = "new_time_period")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_time_period")]
     public static extern
     IntPtr NewTimePeriod(int days_per_year);
 
-    [DllImport(LibOp_dll, EntryPoint = "new_time_period_365d")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_time_period_365d")]
     public static extern
     IntPtr NewTimePeriod365();
 
-    [DllImport(LibOp_dll, EntryPoint = "new_time_period_252d")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_time_period_252d")]
     public static extern
     IntPtr NewTimePeriod252();
 
-    [DllImport(LibOp_dll, EntryPoint = "delete_time_period")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "delete_time_period")]
     public static extern
     IntPtr DeleteTimePeriod();
 
-    [DllImport(LibOp_dll, EntryPoint = "tp_set_days")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tp_set_days")]
     public static extern
     int TimePeriodSetDays(IntPtr time_period, int days);
 
-    [DllImport(LibOp_dll, EntryPoint = "tp_set_years")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tp_set_years")]
     public static extern
     int TimePeriodSetYears(IntPtr time_period, int years);
 
-    [DllImport(LibOp_dll, EntryPoint = "tp_get_date")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tp_get_date")]
     public static extern
     double TimePeriodGetDate(IntPtr time_period, int days);
 
@@ -203,72 +203,66 @@ namespace LibOpCS
      * dividend.h
      */
 
-    [DllImport(LibOp_dll, EntryPoint = "new_continuous_dividend")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_continuous_dividend")]
     public static extern
     IntPtr NewContinuousDividend(double cont_dividend);
 
-    [DllImport(LibOp_dll, EntryPoint = "new_discrete_dividend")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_discrete_dividend")]
     public static extern
-    IntPtr NewContinuousDividend();
+    IntPtr NewDiscreteDividend();
 
-    [DllImport(LibOp_dll, EntryPoint = "delete_dividend")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "delete_dividend")]
     public static extern
     void DeleteDividend(IntPtr dividend);
-
-    // Variadic functions
-    // Argument "size" must be the length of the __arglist
-    // Examples: 
-    //  1) DisDivSetDates(div, tp, 1, __arglist(182));
-    //  2) DiscDivSetAmmounts(div, 2, __arglist(1, 2));
     
-    [DllImport(LibOp_dll, EntryPoint = "div_disc_set_dates")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "div_disc_set_dates_")]
     public static extern
-    void DiscDivSetDates(IntPtr dividend, IntPtr time_period, 
-                          int size, __arglist);
+    int DiscDivSetDates(IntPtr dividend, IntPtr time_period, 
+                        int size, int[] dates);
 
-    [DllImport(LibOp_dll, EntryPoint = "div_disc_set_ammounts")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "div_disc_set_ammounts_")]
     public static extern
-    void DiscDivSetAmmounts(IntPtr dividend, int size, __arglist);
+    int DiscDivSetAmmounts(IntPtr dividend, int size, 
+                            double[] ammounts);
 
     /**
      * result.h
      */
 
-    [DllImport(LibOp_dll, EntryPoint = "new_result")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "new_result")]
     public static extern
     IntPtr NewResult();
 
-    [DllImport(LibOp_dll, EntryPoint = "delete_result")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "delete_result")]
     public static extern
     void DeleteResult(IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "result_get_price")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "result_get_price")]
     public static extern
     double ResultGetPrice(IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "result_get_delta")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "result_get_delta")]
     public static extern
     double ResultGetDelta(IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "result_get_gamma")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "result_get_gamma")]
     public static extern
     double ResultGetGamma(IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "result_get_theta")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "result_get_theta")]
     public static extern
     double ResultGetTheta(IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "result_get_rho")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "result_get_rho")]
     public static extern
     double ResultGetRho(IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "result_get_vega")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "result_get_vega")]
     public static extern
     double ResultGetVega(IntPtr result);
 
-    [DllImport(LibOp_dll, EntryPoint = "result_get_impl_vol")]
+    [DllImport(LibOp_dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "result_get_impl_vol")]
     public static extern
     double ResultGetImpliedVolatility(IntPtr result);
-
   }
 }
